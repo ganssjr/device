718bcd07c0805ed22577b5e41e8b98c02ff6064e
root <root@instance-1.us-central1-a.c.qwiklabs-gcp-03-5e4134ef7f15.internal>
2022-03-07 14:40:22 +0000

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/octavi_rova.mk
+
+COMMON_LUNCH_CHOICES := \
+    octavi_rova-user \
+    octavi_rova-userdebug \
+    octavi_rova-eng
